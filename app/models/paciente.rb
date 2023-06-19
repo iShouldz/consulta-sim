703490c0d8737982b4c8ba42b57fe1824b63be58
Pedro Souza <pedrosport10@gmail.com>
2023-06-19 14:22:50 -0300

    Criando as validations de Paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -3,4 +3,12 @@ class Paciente < ApplicationRecord
   accepts_nested_attributes_for :endereco, allow_destroy: true
   has_many :consultums, :dependent => :delete_all
   has_many :medicos, through: :consultums
+
+  validates :cpf, presence: {message: 'POR FAVOR, DIGITE O CPF'},
+            length: {is: 11, message: 'CPF PRECISA DE 11 DIGITOS'},
+            format: {with: /\A\d+\z/, message: 'APENAS NÚMEROS!'}
+  validates :email, format:{with: URI:: MailTo::EMAIL_REGEXP}
+  validates :nome, presence: {message: 'POR FAVOR, DIGITE SEU NOME'},
+            length: {maximum: 100, message: 'TAMANHO MAXIMO: 100 CARACTERES'}
+
 end
