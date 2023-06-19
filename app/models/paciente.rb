f2539f7e4e2c0bb836c89000bcde44c7d0ac30c7
Pedro Souza <pedrosport10@gmail.com>
2023-06-19 16:45:01 -0300

    Adicionando validação extra para paciente: data de nascimento

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -10,5 +10,12 @@ class Paciente < ApplicationRecord
   validates :email, format:{with: URI:: MailTo::EMAIL_REGEXP}
   validates :nomeCompleto, presence: {message: 'POR FAVOR, DIGITE SEU NOME'},
             length: {maximum: 100, message: 'TAMANHO MAXIMO: 100 CARACTERES'}
+  validates :dataDeNascimento, presence: true
+  validate :validar_data
 
+  def validar_data
+    if dataDeNascimento.present? && dataDeNascimento > Date.today
+      errors.add(:data, "DATA DE NASCIMENTO NÃO PODE SER SUPERIOR A DATA DE HOJE")
+    end
+  end
 end
