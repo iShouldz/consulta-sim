f99f212924cb3eb78bc1c5a815e9776fdded9134
Pedro Souza <pedrosport10@gmail.com>
2023-06-19 15:23:59 -0300

    Criando as validations e associations de endereço

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,2 +1,12 @@
 class Endereco < ApplicationRecord
+  belongs_to :Paciente
+
+  validates :cep, presence: true, length: {is: 8}
+  validates :cidade, presence: true, length: {minimum: 3, too_short: "%{count} Minimo de caracteres",
+                                              maximum: 50, too_long: "%{count} Maximo de caracteres"}
+  validates :bairro, presence: true, length: {minimum: 3, too_short: "%{count} Minimo de caracteres",
+                                              maximum: 20, too_long: "%{count} Maximo de caracteres"}
+  validates :logradouro, presence: true, length: {minimum: 3, too_short: "%{count} Minimo de caracteres",
+                                                  maximum: 60, too_long: "%{count} Maximo de caracteres"}
+  validates :complemento, length: {maximum: 50, too_long: "%{count} maximo de caracteres"}
 end
