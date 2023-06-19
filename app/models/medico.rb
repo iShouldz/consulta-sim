a53ba28bfbac32342380f55833dea86f0f6f1829
Pedro Souza <pedrosport10@gmail.com>
2023-06-19 14:29:38 -0300

    Criando as validations de médico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,4 +1,10 @@
 class Medico < ApplicationRecord
   has_many :consultums, dependent: :destroy
   has_many :pacientes, through: :consultums
+
+  validates :nome, presence: true, numericality: false, uniqueness: false
+  validates :cpf, presence: true, uniqueness: true, length: {is: 11}
+  validates :crm, presence: true, uniqueness: true, length: {minimum: 6}
+  validates :email, presence: true, uniqueness: true, length: {minimum: 5}
+  validates :especialidade, presence: false, uniqueness: false, numericality: false
 end
