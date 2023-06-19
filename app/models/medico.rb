ba79fce4a79dbe013346fc96112fe874f95c35f2
Pedro Souza <pedrosport10@gmail.com>
2023-06-19 15:39:36 -0300

    Modificação validations de medico, 'nome' para 'nomeCompleto'

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -2,7 +2,7 @@ class Medico < ApplicationRecord
   has_many :consultums, dependent: :destroy
   has_many :pacientes, through: :consultums
 
-  validates :nome, presence: true, numericality: false, uniqueness: false
+  validates :nomeCompleto, presence: true, numericality: false, uniqueness: false
   validates :cpf, presence: true, uniqueness: true, length: {is: 11}
   validates :crm, presence: true, uniqueness: true, length: {minimum: 6}
   validates :email, presence: true, uniqueness: true, length: {minimum: 5}
