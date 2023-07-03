74d23c21bcbc6460ae194c46ddde8a0b55f99611
Pedro Souza <pedrosport10@gmail.com>
2023-07-03 18:03:41 -0300

    Adicionando testes de model: paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -4,4 +4,14 @@ class PacienteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'Cadastrar paciente invalido' do
+    paciente = Paciente.new
+    assert_not paciente.save
+  end
+
+  test 'Cadastrar paciente com dados validos' do
+    paciente = Paciente.new nomeCompleto: 'Pedro Caitano', dataDeNascimento: "2000-09-14",
+                            cpf: "05621812450", email: "pedroshouldz@outlook.com"
+    assert paciente.save
+  end
 end
